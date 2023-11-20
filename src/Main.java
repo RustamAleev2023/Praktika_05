8cf87d49834d9a55ef9784006cf42168b14b9579
RustamAleev2023 <ridermix@yandex.ru>
2023-11-20 12:07:28 +0200

    Task 3 done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //       task1();
-       task2();
+//       task2();
+       task3();
     }
 
     //Task1
@@ -41,6 +42,48 @@ public class Main {
         } else {
             System.out.println("Вы ввели некорректную операцию");
         }
+    }
 
+    //Task3
+    public static void task3(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int num2 = scanner.nextInt();
+        System.out.println("Введите третье число");
+        int num3 = scanner.nextInt();
+        System.out.println("Выберите действие (минимум/максимум/среднеарифметическое)");
+        String action = scanner.next();
+
+        if (action.equalsIgnoreCase("минимум")){
+            int min = Integer.MAX_VALUE;
+            if(num1 < min){
+                min = num1;
+            }
+            if(num2 < min){
+                min = num2;
+            }
+            if(num3 < min){
+                min = num3;
+            }
+            System.out.println(min);
+        } else if(action.equalsIgnoreCase("максимум")){
+            int max = Integer.MIN_VALUE;
+            if(num1 > max){
+                max = num1;
+            }
+            if(num2 > max){
+                max = num2;
+            }
+            if(num3 > max){
+                max = num3;
+            }
+            System.out.println(max);
+        } else if (action.equalsIgnoreCase("среднеарифметическое")){
+            System.out.println((num1 + num2 + num3) / 3);
+        } else {
+            System.out.println("Вы ввели некорректную операцию");
+        }
     }
 }
